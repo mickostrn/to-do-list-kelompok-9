a368bf5e314e55b6a42bc0f58ae59aac4acc1600
mickostrn <mickostrn@gmail.com>
2026-08-23 20:14:59 +0700

    Revert "Feature/clear all"

--- js/clearAll.js
+++
@@ -1,9 +0,0 @@
-import { notes, saveToLocalStorage, render } from "./render.js";
-
-export function clearAll() {
-  if (confirm("Yakin ingin menghapus semua?")) {
-    notes.length = 0; // Hapus total isi array
-    saveToLocalStorage();
-    render();
-  }
-}
