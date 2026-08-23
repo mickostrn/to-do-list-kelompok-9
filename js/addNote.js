489c733ef8fff41e1177554e1e101f41f48fad5f
mickostrn <mickostrn@gmail.com>
2026-08-23 19:31:34 +0700

    refactor: mengubah fungsi add-note menjadi file terpisah

---
+++ js/addNote.js
@@ -0,0 +1,15 @@
+import { notes, saveToLocalStorage, render } from "./render.js";
+
+export function addNote() {
+  const inputNote = document.getElementById("inputNote");
+  const text = inputNote.value.trim();
+
+  if (text === "") return alert("Note tidak boleh kosong!");
+
+  const newNote = { id: Date.now(), text: text, isCompleted: false };
+  notes.push(newNote);
+  
+  saveToLocalStorage();
+  render();
+  inputNote.value = "";
+}
